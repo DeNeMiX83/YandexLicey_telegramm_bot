73c6727bb7b0ed126039f1ad5be033211a22a7dd
DeNeMiX83 <60064274+DeNeMiX83@users.noreply.github.com>
2021-04-26 16:36:03 +0300

    Delete j.py

--- j.py
+++
@@ -1,5 +0,0 @@
-{"file_name": "sticker_S.pdf", "mime_type": "application/pdf", "thumb": {
-    "file_id": "AAMCAgADGQEAAhx_YIZ17vcf7HUp0w9K1p1tL-9OxKQAAhENAAIddjlILK6IKX4lBUyghbWiLgADAQAHbQADDmoAAh8E",
-    "file_unique_id": "AQADoIW1oi4AAw5qAAI", "file_size": 5063, "width": 162, "height": 73},
- "file_id": "BQACAgIAAxkBAAIcf2CGde73H-x1KdMPStadbS_vTsSkAAIRDQACHXY5SCyuiCl-JQVMHwQ",
- "file_unique_id": "AgADEQ0AAh12OUg", "file_size": 715152}
