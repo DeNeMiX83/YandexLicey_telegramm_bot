7a4af28cc18bafa24fae95b298aef4c76ef70fd0
daniil <daniil5389@gmail.com>
2021-04-26 16:35:18 +0300

    убрал все лишнее

--- db/connect.py
+++ db/connect.py
@@ -5,7 +5,3 @@ session = db_session.create_session()
 roles = {'none': 'Нету',
         'chief': 'Начальник',
         'subordinate': 'Подчиненный'}
-# for tag, name in roles.items():
-#    role = Roles(tag=tag, name=name)
-#    session.add(role)
-# session.commit()
